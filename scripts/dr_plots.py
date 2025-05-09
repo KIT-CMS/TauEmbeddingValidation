Added muon matching for mvis calculation, fixed dy_max bug, adjusted some bins

scripts/dr_plots.py
@@ -30,7 +30,7 @@ print("Data loaded and verified")
 dr_unfiltered = calculate_dr(data_df, emb_df, 2, 5, filter=False)
 dr_filtered = calculate_dr(data_df, emb_df, 2, 5, filter=True)
 
-ax = q_comparison(dr_filtered[:,0,0], dr_unfiltered[:,0,0], 25, r"$\Delta r_\text{filtered}$", r"$\Delta r_\text{unfiltered}$", r"$\Delta r$")
+ax = q_comparison(dr_filtered[:,0,0], dr_unfiltered[:,0,0], np.linspace(0,6), r"$\Delta r_\text{filtered}$", r"$\Delta r_\text{unfiltered}$", r"$\Delta r$")
 ax[0].set_yscale("log")
 plt.savefig(os.path.join(dr_plot_path, f"dr_comparison.png"))
 plt.close()

scripts/importer.py
@@ -79,27 +79,29 @@ get_subarray_length = np.vectorize(get_subarray_length, otypes=[np.int32]) #func
 def get_z_m_pt(df):
     #finds for each event the muon pair that fits best to the z boson mass. returns arrays with the mass and pt of the best fitting pair
     m_z = 91.1880
-    m_vis = np.full(len(df), np.nan)
-    pt_vis = np.full(len(df), np.nan)
     n_muon = get_nmuon(df, "eta_")
+    n_events = len(df)
+    m_vis = np.full((n_events,n_muon,n_muon), np.nan)
+    pt_vis = np.full((n_events,n_muon,n_muon), np.nan)
 
-    for n_event in range(len(df)):
-        row = df.iloc[n_event]
-        best_m_vis = -1.
-        best_pt_vis = -1.
-        for n1 in range(1, n_muon+1):
-            for n2 in range(1, n_muon+1):
-                m_vis = generate_m_vis(row[f"pt_{n1}"], row[f"eta_{n1}"], row[f"phi_{n1}"], row[f"m_{n1}"], row[f"pt_{n2}"], row[f"eta_{n2}"], row[f"phi_{n2}"], row[f"m_{n2}"])
-                pt_vis = generate_pt_vis(row[f"pt_{n1}"], row[f"eta_{n1}"], row[f"phi_{n1}"], row[f"m_{n1}"], row[f"pt_{n2}"], row[f"eta_{n2}"], row[f"phi_{n2}"], row[f"m_{n2}"])
+    #calculates mvis and pt for each muon pair
+    for n1 in range(1, n_muon+1):
+        for n2 in range(1, n_muon+1):
+            m_vis[:,n1-1, n2-1] = generate_m_vis(df[f"pt_{n1}"], df[f"eta_{n1}"], df[f"phi_{n1}"], df[f"m_{n1}"], df[f"pt_{n2}"], df[f"eta_{n2}"], df[f"phi_{n2}"], df[f"m_{n2}"])
+            pt_vis[:,n1-1, n2-1] = generate_pt_vis(df[f"pt_{n1}"], df[f"eta_{n1}"], df[f"phi_{n1}"], df[f"m_{n1}"], df[f"pt_{n2}"], df[f"eta_{n2}"], df[f"phi_{n2}"], df[f"m_{n2}"])
+    
+    #calculates difference from z boson mass
+    z_difference = np.absolute(np.copy(m_vis-m_z))
 
-                if abs(m_z-m_vis) < abs(m_z-best_m_vis):
-                    best_m_vis = m_vis
-                    best_pt_vis = pt_vis
+    #finds index of best fitting muon pair for each event
+    min_indices = np.nanargmin(z_difference.reshape(n_events, -1), axis=1)
 
-        if best_m_vis != -1.:
-            m_vis[n_event] = best_m_vis
-        if best_pt_vis != -1.:
-            pt_vis[n_event] = best_pt_vis
+    #converts previously reshaped array into to 2d array
+    row_col_indices = np.array([np.unravel_index(idx, (n_muon, n_muon)) for idx in min_indices])
+
+    #extracts values of ideal muon pairs
+    m_vis = m_vis[np.arange(n_events), row_col_indices[:, 0], row_col_indices[:, 1]]
+    pt_vis = pt_vis[np.arange(n_events), row_col_indices[:, 0], row_col_indices[:, 1]]
     
     return m_vis, pt_vis
 
@@ -172,7 +174,7 @@ def calculate_dr(data_df, emb_df, n_data, n_emb, filter):
                 dr_temp[mask] = np.nan
 
                 #only respecting distances smaller this threshold because everything else must be a mismatch
-                dr_temp[dr_temp>0.1] = np.nan
+                dr_temp[dr_temp>6] = np.nan
 
 
             dr_arr[:, n_d-1, n_e-1] = dr_temp

scripts/plotting.py
@@ -57,7 +57,7 @@ def control_plot(data_col, emb_col, bins, title):
     ax_temp.bar(bins_data_center, 2*rel_diff_error, width=np.diff(edges), bottom=1-rel_diff_error, color="grey", alpha=0.5, edgecolor="none")
 
     #adding label and helping line to plot
-    dy_max = get_dy_max(rel_diff)
+    dy_max = get_dy_max(rel_diff-1)
     ax_temp.set_ylim(1-dy_max, 1+dy_max)
     ax_temp.set_ylabel(r"$N_\text{data}/N_\text{emb}$")
     plt.axhline(y=1, xmin=0, xmax=1, linestyle="dashed", color="black")
@@ -138,7 +138,7 @@ def q_comparison(col1, col2, bins, col1_label, col2_label, title):
     ax_temp.step(edges[:-1], rel_difference, where="post", color="black")
     ax_temp.bar(bins_data_center, 2*rel_difference_errors, width=np.diff(edges), bottom=rel_difference-rel_difference_errors, color="grey", alpha=0.5, edgecolor="none")
 
-    dy_max = get_dy_max(rel_difference)
+    dy_max = get_dy_max(rel_difference-1)
     ax_temp.set_ylim(1-dy_max, 1+dy_max)
 
     #adding label and helping line to plot
@@ -152,7 +152,10 @@ def q_comparison(col1, col2, bins, col1_label, col2_label, title):
 def get_dy_max(array):
     not_nan_array = array[~np.isnan(array)]
     array_max = np.amax(np.absolute(not_nan_array))
-    return min(1, 1.5*array_max)
+    dy_max = min(1, 1.5*array_max)
+    if dy_max == 0:
+        return 1
+    return dy_max
 
 
 def x_vs_y(x, y, xlabel, ylabel):